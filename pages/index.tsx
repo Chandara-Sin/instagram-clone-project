97411496ac3d63e723e550bf47338cd0efddd5cf
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-03-31 16:56:38 +0700

    refactor: check session login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,48 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
 import ModalDialog from '../components/ModalDialog'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    session === null && router.push('/auth/signin')
+  }, [])
+
   return (
     <div className="">
       <Head>
         <title>Instagram-personal-project</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          type="image/png"
+          sizes="16x16"
+          rel="icon"
+          href="/icons8-instagram-16.png"
+        />
+        <link
+          type="image/png"
+          sizes="32x32"
+          rel="icon"
+          href="/icons8-instagram-32.png"
+        />
+        <link
+          type="image/png"
+          sizes="96x96"
+          rel="icon"
+          href="/icons8-instagram-96.png"
+        />
+        <link
+          type="image/png"
+          sizes="120x120"
+          rel="icon"
+          href="/icons8-instagram-120.png"
+        />
       </Head>
 
       <Header />
